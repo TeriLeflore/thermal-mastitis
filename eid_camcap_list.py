5d7c839acfcd6eedeb4f490ac7ffec756eead3bf
TeriLeflore <146109949+TeriLeflore@users.noreply.github.com>
2024-02-15 09:56:21 -0500

    Update eid_camcap_list.py

--- eid_camcap_list.py
+++ eid_camcap_list.py
@@ -13,6 +13,7 @@ portname = '/dev/ttyUSB0'
 baudrate = 9600
 timeout = 1
 
+#OAK-D pipeline
 pipeline = dai.Pipeline()
 
 camRgb = pipeline.createColorCamera()
@@ -27,6 +28,8 @@ camRgb.video.link(xoutRgb.input)
 oak_camera = dai.Device(pipeline)
 qRGB = oak_camera.getOutputQueue(name="rgb", maxSize=10, blocking=False)
 
+
+#Videocapture devices (thermal)
 cap0 = cv2.VideoCapture("/dev/v4l/by-id/usb-FLIR_Boson_196769-video-index0")
 cap1 = cv2.VideoCapture("/dev/v4l/by-id/usb-FLIR_Boson_97206-video-index0")
 cap2 = cv2.VideoCapture("/dev/v4l/by-id/usb-FLIR_Boson_196764-video-index0")
@@ -51,18 +54,21 @@ async def run():
         img_name = "_".join(eid_list) + ".tiff"
         print(img_name)
 
+	#FLIR Boson camera 769 (24mm)
         ret0, frame0 = cap0.read()
         if ret0: 
               cv2.imshow('Cam 0',frame0)
               cv2.imwrite("B769"+ img_name, frame0)
         cap0.release()
-
+        
+	#FLIR Boson camera 206 (6mm)
         ret1, frame1 = cap1.read()
         if ret1:
               cv2.imshow('Cam 1',frame1)
               cv2.imwrite("B206" + img_name, frame1)     
         cap1.release()
 
+	#FLIR Boson camera 764
         ret2, frame2 = cap2.read()
         if ret2:
               cv2.imshow('Cam 2',frame2)
@@ -73,17 +79,7 @@ async def run():
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
 
-          
-       # results = []
-        #for vidcap in vid_capdevices:
-          # ret, frame = vidcap.read()
-          # results.append( [ret,frame] )
-       # for number, (ret, frame) in enumerate(results):
-       #    if ret:
-        #      cv2.imwrite(img_name, frame)
-        #      #cv2.imshow(f'Cam {number}', frame)
-        #vid_capdevices.release()
-
+	#OAK-D 
         inRgb = qRGB.get()
         frame = inRgb.getCvFrame()
         cv2.imwrite(f"{img_name}_Rgb.png", frame)
