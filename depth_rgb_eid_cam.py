5da283cc4078c73e754e8ce8ba5ecdf07423a20f
TeriLeflore <146109949+TeriLeflore@users.noreply.github.com>
2024-03-05 10:08:29 -0500

    Update depth_rgb_eid_cam.py

--- depth_rgb_eid_cam.py
+++ depth_rgb_eid_cam.py
@@ -1,8 +1,4 @@
 # This is to test adding oak depth/disparity photos to code. It has not been tested to see if it works
-#created 3/4/24
-# This is to test adding oak depth/disparity and rgb photos to OAK-D to see if works before adding to eid panel code
-#it works!
-#tested code on 3/5, the depth and disparity photos aren't showing even though it does capture the image . RGB is the only one that works.
 
 import datetime
 from asyncio import get_event_loop
@@ -14,6 +10,15 @@ from serial_asyncio import open_serial_connection
 import numpy as np
 #import hashlib
 
+# Tag reader variables
+portname = '/dev/ttyUSB0'
+baudrate = 9600
+timeout = 1
+
+# IR camera variables
+ir_camera_list = [("B769","/dev/v4l/by-id/usb-FLIR_Boson_196769-video-index0"),
+		("B206","/dev/v4l/by-id/usb-FLIR_Boson_97206-video-index0"),
+		("B764","/dev/v4l/by-id/usb-FLIR_Boson_196764-video-index0")]	
 
 def getFrame(queue):
   # Get frame from queue
@@ -110,37 +115,65 @@ if __name__ == '__main__':
     
     camRgb.video.link(xoutRgb.input) #linking
 
-    
-    # Pipeline is defined, now we can connect to the device
 
-    with dai.Device(pipeline) as device:
+# define event loop
+async def run():
+    reader, writer = await open_serial_connection(url=portname, baudrate=baudrate)
 
+    tmpData=serial.Serial('/dev/ttyACM0', 115200)
+    time.sleep(1)
+
+    while True:
+        # when Cattle tag is read create a tag and timestamp string
+        eid = await reader.readline()
+        eid_list = [eid.decode().strip()]
+        datetime_list = str(datetime.datetime.now()).split(" ")
+        eid_list.extend(datetime_list)
+        # temp, pres, humidity data
+        dataPacket=tmpData.readline()
+        dataPacket=str(dataPacket,'utf-8')
+        dataPacket=dataPacket.strip('\r\n')
+        eid_list.extend(dataPacket)
+        img_name = "_".join(eid_list)
+        print(img_name)
+	
+	# Loop through the IR cameras collecting 1 unscaled 16-bit and scaled 8-bit images
+        for camera in ir_camera_list:
+            camobj = cv2.VideoCapture(camera[1])
+            for mode in ["16bit", "8bit"]:
+            	caminst = set_mode(camobj, mode)
+            	ret0, frame0 = caminst.read()
+            	if ret0: 
+                    #cv2.imshow(camera[0], frame0) # uncomment for image preview screen
+                    cv2.imwrite(camera[0] + "_" + mode + "_" + img_name + ".tiff", frame0)
+            camobj.release()
+ 
+	#Aquire Color images from OAK-D cameras
+        oak_cam_list = dai.Device.getAllAvailableDevices()
+        for oakcam in oak_cam_list:
+            device_info = dai.DeviceInfo(oakcam.mxid)
+            oak_camera = dai.Device(pipeline, device_info)
+            disparityQueue = oak_camera.getOutputQueue(name="disparity", maxSize=1, blocking=False)
+            rectifiedLeftQueue = oak_camera.getOutputQueue(name="rectifiedLeft", maxSize=1, blocking=False)
+            rectifiedRightQueue = oak_camera.getOutputQueue(name="rectifiedRight", maxSize=1, blocking=False)
+            rgbQueue = oak_camera.getOutputQueue(name="rgb", maxSize=1, blocking=False)
         
-        # Output queues will be used to get the rgb frames and nn data from the outputs defined above
-        disparityQueue = device.getOutputQueue(name="disparity", maxSize=1, blocking=False)
-        rectifiedLeftQueue = device.getOutputQueue(name="rectifiedLeft", maxSize=1, blocking=False)
-        rectifiedRightQueue = device.getOutputQueue(name="rectifiedRight", maxSize=1, blocking=False)
-        rgbQueue = device.getOutputQueue(name="rgb", maxSize=1, blocking=False)
+            # Calculate a multiplier for colormapping disparity map
+            disparityMultiplier = 255 / stereo.initialConfig.getMaxDisparity()
 
-
-        # Calculate a multiplier for colormapping disparity map
-        disparityMultiplier = 255 / stereo.initialConfig.getMaxDisparity()
-
-        cv2.namedWindow("Stereo Pair")
+            cv2.namedWindow("Stereo Pair")
         
-        # Variable use to toggle between side by side view and one frame view.
-        sideBySide = False
-
-        while True:
-            
-            # Get disparity map
+            # Variable use to toggle between side by side view and one frame view.
+            sideBySide = False
+        
+                # Get disparity map
             disparity = getFrame(disparityQueue)
             
             # Colormap disparity for display
             disparity = (disparity * disparityMultiplier).astype(np.uint8)
             disparity = cv2.applyColorMap(disparity, cv2.COLORMAP_JET)
             
-            # Get left and right rectified frame
+                # Get left and right rectified frame
             leftFrame = getFrame(rectifiedLeftQueue);
             rightFrame = getFrame(rectifiedRightQueue)
             
@@ -156,26 +189,37 @@ if __name__ == '__main__':
             
             cv2.imshow("Stereo Pair", imOut)
             cv2.imshow("Disparity", disparity)
-            cv2.imwrite("stereo_pair" + ".png", imOut)
-            cv2.imwrite("disparity" + ".png", disparity)            
+            #cv2.imwrite("stereo_pair" + ".png", imOut)
+            #cv2.imwrite("disparity" + ".png", disparity) 
+            cv2.imwrite(oakcam.mxid + "_stereo_pair_" + img_name + ".png", imOut) 
+            cv2.imwrite(oakcam.mxid + "_disparity_" + img_name + ".png", disparity)
+            
+                      
             #Aquire Color images from OAK-D cameras
-
             qRGB = getFrame(rgbQueue)
             
-            cv2.imshow("RGB", qRGB)
-            cv2.imwrite("color_image" + ".png", qRGB)
+            #cv2.imshow("RGB", qRGB)
+            cv2.imwrite(oakcam.mxid + "_rgb_" + img_name + ".png", qRGB)
+                #cv2.imwrite("color_image" + ".png", qRGB)
 
-            # Check for keyboard input
+                # Check for keyboard input
             key = cv2.waitKey(1)
             if key == ord('q'):
-                # Quit when q is pressed
+            # Quit when q is pressed
                 break
             elif key == ord('t'):
                 # Toggle display when t is pressed
                 sideBySide = not sideBySide
-            
 
 
+            #file_name = 'home/Mastitis/oakcam.mxid + _rgb_ + img_name + .png'
+            #with open(file_name) as f:
+                #data = f.read()
+                #read_hash = hashlib.sha256(data).hexdigest()
+                #print(read_hash)
+   
 
+    cv2.destroyAllWindows()
 
-  
+loop = get_event_loop()
+loop.run_until_complete(run())
